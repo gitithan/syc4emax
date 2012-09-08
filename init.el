646c4cd420f84ef6aaa9ec27aef23ae2573929d7
gitithan <gitit.han@gmail.com>
2012-09-09 00:51:59 +0800

    [open] init-locales.el

--- init.el
+++ init.el
@@ -163,6 +163,19 @@
    (message "Emacs startup time: %d seconds."
     (time-to-seconds (time-since emacs-load-start-time)))
    )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-drill-optimal-factor-matrix nil)
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(dbgp-response-face ((t (:inherit default)))))
 ;;; Local Variables:
 ;;; no-byte-compile: t
 ;;; End:
